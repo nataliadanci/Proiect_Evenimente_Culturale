7b774515369189cada709ac0da4b93f9248da657
nataliadanci <nataliadanci0@gmail.com>
2024-12-12 20:10:30 +0200

    Added css for the project

--- Models/Registration.cs
+++ Models/Registration.cs
@@ -5,10 +5,14 @@ namespace Proiect_MP1.Models
     public class Registration
     {
         public int ID { get; set; }
+
+        [Display(Name = "Participant")]
         public int? UserID { get; set; }
 
         [Display(Name = "Participant")]
         public User? User { get; set; }
+
+        [Display(Name = "Eveniment")]
         public int? EvenimentID { get; set; }
         public Eveniment? Eveniment { get; set; }
 
